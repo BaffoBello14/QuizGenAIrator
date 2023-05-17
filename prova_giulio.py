6ac66d60c89f4240190e4cde7648821e751cf0b5
Giulio Bello <giuliobello12@gmail.com>
2023-05-17 20:14:25 +0200

    prova con pdf

--- prova_giulio.py
+++ prova_giulio.py
@@ -1,49 +1,72 @@
+import PyPDF2
+import random
 import openai
 
-# Imposta la tua chiave API di OpenAI
-openai.api_key = 'LA_TUA_CHIAVE_API'
+openai.api_key = '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'
 
-def create_quiz(text):
-    # Chiamata all'API di OpenAI per generare domande sul testo di input
+def generate_question_answer(text):
+    # Controlla la lunghezza del testo
+    if len(text) > 4096:
+        text = text[:4096]  # Taglia il testo se supera il limite consentito
+    
+    # Genera una domanda
+    question = "Qual è la seguente affermazione vera?\n" + text
+    
+    # Genera una risposta corretta
+    answer_correct = text
+    
+    # Genera 4 risposte errate
+    answers_wrong = []
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=text,
         max_tokens=100,
-        n=5,
+        n=4,
         stop=None,
         temperature=0.6,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
     )
+    if response.choices:
+        answers_wrong = [choice['text'] for choice in response.choices]
+    
+    # Randomizza l'ordine delle risposte
+    answers = [answer_correct] + answers_wrong
+    random.shuffle(answers)
+    
+    return question, answers
 
-    # Estrai le domande dalla risposta dell'API
-    questions = [choice['text'].strip() for choice in response.choices]
+def process_pdf(file_path):
+    # Apre il file PDF
+    with open(file_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        
+        # Estrae il testo da tutte le pagine
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        # Dividi il testo in domande
+        questions = text.split('?')
+        
+        # Genera domande e risposte per ogni domanda
+        quiz = []
+        for question_text in questions:
+            if question_text.strip() != '':
+                question, answers = generate_question_answer(question_text)
+                quiz.append({
+                    'question': question,
+                    'answers': answers
+                })
+        
+        return quiz
 
-    # Crea il formato del quiz a risposta multipla
-    quiz = []
-    for i, question in enumerate(questions):
-        options = [
-            {'text': 'Risposta corretta', 'correct': True},
-            {'text': 'Risposta errata 1', 'correct': False},
-            {'text': 'Risposta errata 2', 'correct': False},
-            {'text': 'Risposta errata 3', 'correct': False}
-        ]
-        quiz.append({
-            'id': i + 1,
-            'question': question,
-            'options': options
-        })
+# Esempio di utilizzo
+quiz = process_pdf('C:/Users/giuli/OneDrive/Desktop/Cloud Computing/2023_02_09_lecture_notes_21_22_Cloud_Computing_Giacomo_Pacini.pdf')
 
-    return quiz
-
-# Test del codice con un esempio di input
-input_text = "Questo è un esempio di testo su cui creare un quiz a risposta multipla."
-quiz = create_quiz(input_text)
-
-# Stampa il quiz generato
-for question in quiz:
-    print(f"Domanda {question['id']}: {question['question']}")
-    for i, option in enumerate(question['options']):
-        print(f"{chr(65+i)}) {option['text']}")
-    print()
+# Stampa le domande e le risposte generate
+for i, qna in enumerate(quiz):
+    print(f"Domanda {i+1}: {qna['question']}")
+    for j, answer in enumerate(qna['answers']):
+        print(f"{chr(65 + j)}) {answer}")
