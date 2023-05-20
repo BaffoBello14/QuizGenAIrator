b49a32acbdc8d7b28efbf3ab5684b044db4d4f54
JacopoNiccolai <133015363+JacopoNiccolai@users.noreply.github.com>
2023-05-20 19:03:23 +0200

    Updated version with QuizAnalyzer declaration

---
+++ BPM_QuizGeneration_2.0/quiz_class.py
@@ -0,0 +1,56 @@
+from question_class import Question
+
+
+class Quiz:
+    def __init__(self):
+        super().__init__()
+
+        self.questions = []
+
+        file_path = 'results/quiz.txt'
+        with open(file_path, encoding='utf-8') as file:
+            content = file.read()
+
+        # Split the content into individual questions
+        question_texts = content.split('\n\n')
+
+        # Process each question text
+        for question_text in question_texts:
+            question_lines = question_text.strip().split('\n')
+
+            # Extract the question and answers
+            question = question_lines[0][question_lines[0].index('. ') + 2:]
+            answers = [line[2:] for line in question_lines[1:-1]]
+
+            # Extract the correct answer
+            correct_answer = question_lines[-1][-1]
+
+            # Add the question to the quiz object
+            self.add_question(question, answers, correct_answer)
+
+    def add_question(self, question, answers, correct_answer):
+        new_question = Question(question, answers, correct_answer)
+        self.questions.append(new_question)
+
+    def get_question(self, index):
+        if 0 <= index < len(self.questions):
+            return self.questions[index]
+        else:
+            return None
+
+    def print_quiz(self):
+        for i, question in enumerate(self.questions, start=1):
+            print(f"Question {i}:", question.get_text())
+            options = question.get_answers()
+            for j, option in enumerate(options, start=1):
+                print(f"{chr(64 + j)}. {option}")
+            print()
+
+    def print_correct_answers(self):
+        for i, question in enumerate(self.questions, start=1):
+            print(f"Correct answer for question {i}:", question.get_correct_answer())
+        print()
+
+    def get_num_questions(self):
+        return len(self.questions)
+
