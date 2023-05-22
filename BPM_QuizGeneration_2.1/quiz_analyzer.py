a9626f4dfbf984a6c400977de654b36b5dad800d
Giulio Bello <giuliobello12@gmail.com>
2023-05-22 11:34:49 +0200

    quiz analyzer 22/05 11:30

--- BPM_QuizGeneration_2.1/quiz_analyzer.py
+++ BPM_QuizGeneration_2.1/quiz_analyzer.py
@@ -1,21 +1,19 @@
 import spacy
 
-
 class QuizAnalyzer:
     def __init__(self, quiz, text):
         super().__init__()
-
-        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy.load("en_core_web_lg")  # Utilizzo di un modello più specifico
         self.quiz = quiz
         self.text = text
 
     def extract_questions_keywords(self):
-
         for i in range(self.quiz.get_num_questions()):
-            # Process the text with spaCy
-            doc = self.nlp(self.quiz.get_question(i).get_text())
+            question_text = self.quiz.get_question(i).get_text()
+            # Process the question text with spaCy
+            doc = self.nlp(question_text)
 
-            text_entities = doc.ents
+            question_entities = doc.ents
 
             # Extract the keywords
             keywords = []
@@ -23,14 +21,13 @@ class QuizAnalyzer:
                 if chunk.root.pos_ == "NOUN":
                     keywords.append(chunk.text)
 
-            # Print the keywords
-            print(self.quiz.get_question(i).get_text())
-            print(keywords)
-            print(text_entities)
+            # Print the question, keywords, and entities
+            print(question_text)
+            print("Keywords:", keywords)
+            print("Entities:", question_entities)
         print()
 
     def analyze_starting_text(self):
-
         text_doc = self.nlp(self.text)
 
         # Named Entities: Extract named entities (people, organizations, locations, etc.)
@@ -43,3 +40,17 @@ class QuizAnalyzer:
         print("Text Keywords")
         print(text_keywords)
         print()
+
+    def compare_text_quiz(self):
+        # Implementa una logica di confronto testo-quiz
+        text_doc = self.nlp(self.text)
+
+        for i in range(self.quiz.get_num_questions()):
+            question_text = self.quiz.get_question(i).get_text()
+            question_doc = self.nlp(question_text)
+
+            similarity_score = text_doc.similarity(question_doc)  # Calcola la similarità del coseno
+            print("Question:", question_text)
+            print("Similarity Score:", similarity_score)
+            print()
+
